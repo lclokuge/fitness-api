9dd56b62a982a47f10e3a0d0852d143400d49aa5
lclokuge <lclokuge@gmail.com>
2023-05-15 00:09:46 +0100

    changed JWT

---
+++ database/migrations/2023_05_14_225513_add_uid_foringkey.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_activities', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('date');
+            $table->string('duration');
+            $table->string('calories');
+            $table->string('distance');
+            $table->string('note');
+            $table->timestamps();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('activity_id')->constrained('activities');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_activities');
+    }
+};
